1e48cb27eb71765a415515a30deb7b4cbc0e25f8
S-e-cmd <ewigkeitein@gmail.com>
2026-08-21 06:56:39 +0900

    Keep release metadata aligned with core version

--- plugins/garden-opening-status/garden-opening-status.php
+++ plugins/garden-opening-status/garden-opening-status.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: 開催情報・開催状況管理
  * Description: 春・秋・冬の開催概要を一元管理し、各会期ページとトップページの開催状況へ共通出力します。
- * Version: 3.4.0
+ * Version: 3.3.2
  * Author: Site Admin
  * Requires at least: 5.8
  * Requires PHP: 7.4
